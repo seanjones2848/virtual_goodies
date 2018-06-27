ea9877372ccfe6d585b5ff6eaa9c8f9c2c3bcfea
Sean Jones <sean@AD.HIGHFIDELITY.IO>
2018-06-27 13:32:08 -0700

    added model description to magicWandEntity

--- Objects/magic_wand/magicWandSpawner.js
+++ Objects/magic_wand/magicWandSpawner.js
@@ -5,20 +5,15 @@ orientation = Quat.fromVec3Degrees(orientation);
 var center = Vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getFront(orientation)));
 
 var SCRIPT_URL = Script.resolvePath("magicWandEntityScript.js");
+var MODEL_URL = Script.resolvePath("https://rawgit.com/seanjones2848/virtual_goodies/master/Objects/magic_wand/magic_wand.fbx");
 var magicWand = Entities.addEntity({
-    type: "Shape",
-    shape: "Cylinder",
+    type: "Model",
     name: "magic-wand",
     position: center,
-    color: {
-        r: 255,
-        g: 255,
-        b: 255
-    },
     dimentions: {
-        x: 0.02,
-        y: 0.2,
-        z: 0.02
+        x: 1.0,
+        y: 1.0,
+        z: 1.0
     },
     script: SCRIPT_URL,
     userdata: JSON.stringify({
