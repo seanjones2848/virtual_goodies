e70a06149158f90ee0394203765214d7b6ed1b6e
Sean Jones <sean@AD.HIGHFIDELITY.IO>
2018-06-29 11:07:10 -0700

    cleaned up and working with pic and json file

--- Objects/magic_wand/magicWandEntityScript.js
+++ Objects/magic_wand/magicWandEntityScript.js
@@ -1,238 +1,227 @@
 (function() {
-    //Script.include("../../../developer/libraries/utils.js");
+    Script.include("../../../developer/libraries/utils.js");
+    print("we in the scripty!!!")
     var _this;
     var TRIGGER_CONTROLS = [
         Controller.Standard.LT,
         Controller.Standard.RT,
     ];
     var triggerState = false;
-    var TIMESCALE = 0.03;
-    var ACTION_TTL = 10;    
-
-    MagicWand = function() {
+    var launchSound = SoundCache.getSound("https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/missle+launch.wav");
+    var explosionSound = SoundCache.getSound("https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/fireworksExplosion.wav");
+    var TIME_TO_EXPLODE = 3000;
+    var RELOAD_THRESHOLD = 0.9;
+    var FIREBALL_FORCE = 10;
+    var WAND_TIP_OFFSET = 0.16;
+    
+    function MagicWand() {
         _this = this;
-        _this.launchSound = SoundCache.getSound("https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/missle+launch.wav");
-        _this.explosionSound = SoundCache.getSound("https://s3-us-west-1.amazonaws.com/hifi-content/eric/Sounds/fireworksExplosion.wav");
-        _this.TIME_TO_EXPLODE = 3000;
-    };
-
-    function triggerPulled(hand) {
-        var triggerValue = Controller.getValue(TRIGGER_CONTROLS[hand]);
-        var oldTriggerState = triggerState;
-        var TRIGGER_PULL_THRESHOLD = 0.5;
-        var TRIGGER_RELEASE_THRESHOLD = 0.4;
-        if (triggerValue > TRIGGER_PULL_THRESHOLD) {
-            triggerState = true;
-        } else if (triggerValue < TRIGGER_RELEASE_THRESHOLD) {
-            triggerState = false;
-        }
-        return (triggerState && (oldTriggerState != triggerState));
-    };
-
-    function updateSpringAction(timescale) {
-        var targetProps = SVGElementInstanceList.getEntityProperties(_this.entityID);
-        var props = {
-            targetPosition: targetProps.position,
-            targetRotation: targetProps.rotation,
-            linearTimeScale: timescale,
-            angularTimeScale: timescale,
-            ttl: ACTION_TTL
-        };
-        Entities.updateAction(_this.copy, _this.actionID, props);
-    };
-
-    function createSpringAction(timescale) {
-        var targetProps = SVGElementInstanceList.getEntityProperties(_this.entityID);
-        var props = {
-            targetPosition: targetProps.position,
-            targetRotation: targetProps.rotation,
-            linearTimeScale: timescale,
-            angularTimeScale: timescale,
-            ttl: ACTION_TTL
-        };
-        _this.actionID = Entities.addAction("spring", _this.copy, props);
-    };
-
-    function createCopy() {
-        var origionalProps = Entities.getEntityProperties(_this.entityID);
-        var props = {
-            type: origionalProps.type,
-            color: origionalProps.color,
-            dimentions: origionalProps.dimentions,
-            dynamic: true,
-            damping: 0.0,
-            angularDamping: 0.0,
-            collidesWith: 'dynamic,static,kinematic',
-            rotation: origionalProps.rotation,
-            position: origionalProps.position,
-            shapeType: origionalProps.shapeType,
-            visible: true,
-            userdata: JSON.stringify({
-                grabbableKey: {
-                    grabable: true
-                }
-            })
-        };
-        _this.copy = Entities.addEntity(props);
-    };
-
-    function deleteCopy() {
-        Entities.deleteEntity(_this.copy);
-    };
-
-    function makeOrigionalInvisible() {
-        Entities.editEntity(_this.entityID, {
-            visible: false,
-            collisionless: true
-        })
-    };
-
-    function makeOrigionalVisible() {
-        Entities.editEntity(_this.entityID, {
-            visible: true,
-            collisionless: false
-        })
-    };
-
-    function deleteSpringAction() {
-        Entities.deleteAction(this.copy, this.actionID);
-    };
-
-    function shootFire(launchPosition) {
-        Audio.playSound(_this.launchSound, {
-            position: launchPosition,
-            volume: 0.5
-        });
-        var smoke = Entities.addEntity({
-            type: "ParticleEffect",
-            position: _this.position,
-            velocity: vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getForward(Camera.getOrientation()))),
-            lifespan: 5,
-            lifetime: 10,
-            isEmmiting: true,
-            name: "Smoke Trail",
-            maxParticles: 2000,
-            emitRate: 80,
-            emitSpeed: 0,
-            speedSpread: 0,
-            polarStart: 0,
-            polarFinish: 0,
-            azimuthStart: -Math.PI,
-            azimuthFinish: Math.PI,
-            emitAcceleration: {
-                x: 0,
-                y: 0.01,
-                z: 0
-            },
-            accelerationSpread: {
-                x: 0.01,
-                y: 0,
-                z: 0.01
-            },
-            radiusSpread: 0.03,
-            particleRadius: 0.1,
-            radiusStart: 0.02,
-            radiusFinish: 0.1,
-            alpha: 0.1,
-            alphaSpread: 0,
-            alphaStart: 0.7,
-            alphaFinish: 0,
-            textures: "https://hifi-public.s3.amazonaws.com/alan/Particles/Particle-Sprite-Smoke-1.png",
-            emitterShouldTrail: true,
-        });
-        Script.setTimeout(function() {
-            var explodePosition = Entities.getEntityProperties(smoke, "position").position;
-            explodeFire(explodePosition);
-        }, _this.TIME_TO_EXPLODE);
-    };
-
-    function explodeFire(explodePosition) {
-        Audio.playSound(_this.explosionSound, {
-            position: explodePosition
-        });
-        var firework = Entities.addEntity({
-            name: "fireworks emitter",
-            position: explodePosition,
-            type: "ParticleEffect",
-            colorStart: hslToRgb({
-                h: Math.random(),
-                s: 0.5,
-                l: 0.7
-            }),
-            color: hslToRgb({
-               h: Math.random(),
-               s: 0.5,
-               l: 0.5
-           }),
-           colorFinish: hslToRgb({
-               h: Math.random(),
-               s: 0.5,
-               l: 0.7
-           }),
-           maxParticles: 10000,
-           lifetime: 20,
-           lifespan: randFloat(1.5, 3),
-           emitRate: randInt(500, 5000),
-           emitSpeed: randFloat(0.5, 2),
-           speedSpread: 0.2,
-           emitOrientation: Quat.fromPitchYawRollDegrees(randInt(0, 360), randInt(0, 360), randInt(0, 360)),
-           polarStart: 1,
-           polarFinish: randFloat(1.2, 3),
-           azimuthStart: -Math.PI,
-           azimuthFinish: Math.PI,
-           emitAcceleration: {
-               x: 0,
-               y: randFloat(-1, -0.2),
-               z: 0
-           },
-           accelerationSpread: {
-               x: Math.random(),
-               y: 0,
-               z: Math.random()
-           },
-           particleRadius: randFloat(0.001, 0.1),
-           radiusSpread: Math.random() * 0.1,
-           radiusStart: randFloat(0.001, 0.1),
-           radiusFinish: randFloat(0.001, 0.1),
-           alpha: randFloat(0.8, 1.0),
-           alphaSpread: randFloat(0.1, 0.2),
-           alphaStart: randFloat(0.7, 1.0),
-           alphaFinish: randFloat(0.7, 1.0),
-           textures: "http://ericrius1.github.io/PlatosCave/assets/star.png",
-        });
-
-        Script.setTimeout(function() {
-            Entities.editEntity(firework, {
-                isEmitting: false
-            });
-        }, 1000);
-   };
-
-    this.continueEquip = function(id, params) {
-        var hand = params[0] == "left" ? 0 : 1;
-        if(triggerPulled(hand)) {
-            shootFire(_this.position)
-        }
+        return;
     };
 
     MagicWand.prototype = {
-        preload: function(entityID) {
-            _this.entityID = entityID;
-        },
-        startNearGrab: function(entityID, data) {
-            createCopy();
-            createSpringAction(TIMESCALE);
-            makeOrigionalInvisible();
-        },
-        continueNearGrab: function() {
-            updateSpringAction();
-        },
-        releaseGrab: function() {
-            deleteSpringAction();
-            deleteCopy();
-            makeOrigionalVisible();
-        },
-    };
+        hand: null,
+        canFire: false,
+        canFireTimeout: null,
 
-    return new MagicWand;
-})
+        preload: function(entityID) {
+            this.entityID = entityID;
+            this.launchSound = launchSound;
+            this.explosionSound = explosionSound;
+            //this.createTipEntity(entityID);
+        },
+        createTipEntity: function(entityID) {
+            //for debugging where the tip is
+            var wandProps = Entities.getEntityProperties(this.entityID, ["position", "rotation"]);
+
+            var tip = Entities.addEntity({
+                name: "WandTip",
+                dimensions: {
+                    x: 0.02,
+                    y: 0.02,
+                    z: 0.02
+                },
+                color: {
+                    red: 255,
+                    blue: 0,
+                    green: 0
+                },
+                type: "Box",
+                parentID: entityID,
+                position: this.getWandTipPosition(wandProps),
+            });
+        },
+        getWandTipPosition: function(wandProps) {
+            var Offset = Vec3.multiply(Quat.getUp(wandProps.rotation), WAND_TIP_OFFSET);
+            var WandTipPosition = Vec3.sum(wandProps.position, Offset);
+            return WandTipPosition;
+        },
+        startEquip: function(entityID, args) {
+            print("beginning to equip");
+            this.hand = args[0] == "left" ? 0 : 1;
+        },
+        continueEquip: function(entityID, args) {
+            if (this.canFireTimeout !== null) {
+                Script.clearTimeout(this.canFireTimeout);
+            }
+            this.checkTriggerPressure(this.hand);
+        },
+        releaseEquip: function(entityID, args) {
+            print("let go");
+            var _this = this;
+            this.canFireTimeout = Script.setTimeout(function() {
+                _this.canFire = false;
+            }, 250);
+        },
+        checkTriggerPressure: function(hand) {
+            this.triggerValue = Controller.getValue(TRIGGER_CONTROLS[hand]);
+            if (this.triggerValue < RELOAD_THRESHOLD) {
+                this.canFire = true;
+            } else if (this.triggerValue >= RELOAD_THRESHOLD && this.canFire === true) {
+                var wandProps = Entities.getEntityProperties(this.entityID, ["position", "rotation"]);
+                print("firing");
+                this.shootFire(wandProps);
+                this.canFire = false;
+            }
+            return (triggerState && (oldTriggerState != triggerState));
+        },
+        explode: function(explodePosition) {
+            print("wand: exploding");
+            Audio.playSound(_this.explosionSound, {
+                position: explodePosition
+            });
+            var firework = Entities.addEntity({
+                name: "fireworks emitter",
+                position: explodePosition,
+                type: "ParticleEffect",
+                colorStart: hslToRgb({
+                    h: Math.random(),
+                    s: 0.5,
+                    l: 0.7
+                }),
+                color: hslToRgb({
+                   h: Math.random(),
+                   s: 0.5,
+                   l: 0.5
+               }),
+               colorFinish: hslToRgb({
+                   h: Math.random(),
+                   s: 0.5,
+                   l: 0.7
+               }),
+               maxParticles: 10000,
+               lifetime: 20,
+               lifespan: randFloat(1.5, 3),
+               emitRate: randInt(500, 5000),
+               emitSpeed: randFloat(0.5, 2),
+               speedSpread: 0.2,
+               emitOrientation: Quat.fromPitchYawRollDegrees(randInt(0, 360), randInt(0, 360), randInt(0, 360)),
+               polarStart: 1,
+               polarFinish: randFloat(1.2, 3),
+               azimuthStart: -Math.PI,
+               azimuthFinish: Math.PI,
+               emitAcceleration: {
+                   x: 0,
+                   y: randFloat(-1, -0.2),
+                   z: 0
+               },
+               accelerationSpread: {
+                   x: Math.random(),
+                   y: 0,
+                   z: Math.random()
+               },
+               particleRadius: randFloat(0.001, 0.1),
+               radiusSpread: Math.random() * 0.1,
+               radiusStart: randFloat(0.001, 0.1),
+               radiusFinish: randFloat(0.001, 0.1),
+               alpha: randFloat(0.8, 1.0),
+               alphaSpread: randFloat(0.1, 0.2),
+               alphaStart: randFloat(0.7, 1.0),
+               alphaFinish: randFloat(0.7, 1.0),
+               textures: "http://ericrius1.github.io/PlatosCave/assets/star.png",
+            });
+            Script.setTimeout(function() {
+                Entities.editEntity(firework, {
+                    isEmitting: false
+                });
+            }, 1000);
+        },
+        shootFire: function(wandProps) {
+            print("wand: shooting the fireball");
+            var forwardVec = Quat.getForward(Quat.multiply(wandProps.rotation, Quat.fromPitchYawRollDegrees(0, 180, 0)))
+            forwardVec = Vec3.multiply(Vec3.normalize(forwardVec), FIREBALL_FORCE);
+            Audio.playSound(_this.launchSound, {
+                position: wandProps.position,
+                volume: 0.5
+            });
+            var fireball = Entities.addEntity({
+                name: "fireball",
+                shapeType: "sphere",
+                type: "Sphere",
+                color: {
+                    red: 255,
+                    green: 0,
+                    blue: 0
+                },
+                dimensions: {
+                    x: 0.05,
+                    y: 0.05,
+                    z: 0.05
+                },
+                damping: 0.4,
+                //gravity: {
+                //    x: 0,
+                //    y: -4.8,
+                //    z: 0
+                //},
+                restitution: 0.6,
+                dynamic: true,
+                rotation: wandProps.rotation,
+                position: this.getWandTipPosition(wandProps),
+                velocity: forwardVec,
+                lifetime: 10
+            });
+            var smoke = Entities.addEntity({
+                type: "ParticleEffect",
+                lifespan: 5,
+                lifetime: 10,
+                isEmmiting: true,
+                name: "Smoke Trail",
+                maxParticles: 2000,
+                emitRate: 80,
+                emitSpeed: 0,
+                speedSpread: 0,
+                polarStart: 0,
+                polarFinish: 0,
+                azimuthStart: -Math.PI,
+                azimuthFinish: Math.PI,
+                emitAcceleration: {
+                    x: 0,
+                    y: 0.01,
+                    z: 0
+                },
+                accelerationSpread: {
+                    x: 0.01,
+                    y: 0,
+                    z: 0.01
+                },
+                radiusSpread: 0.03,
+                particleRadius: 0.1,
+                radiusStart: 0.02,
+                radiusFinish: 0.1,
+                alpha: 0.1,
+                alphaSpread: 0,
+                alphaStart: 0.7,
+                alphaFinish: 0,
+                textures: "https://hifi-public.s3.amazonaws.com/alan/Particles/Particle-Sprite-Smoke-1.png",
+                emitterShouldTrail: true,
+                parent: fireball,
+            });
+            Script.setTimeout(function() {
+                var explodePosition = Entities.getEntityProperties(fireball, "position").position;
+                _this.explode(explodePosition);
+            }, TIME_TO_EXPLODE);
+        },      
+    };
+    return new MagicWand();
+});
--- Objects/magic_wand/magicWandSpawner.js
+++ Objects/magic_wand/magicWandSpawner.js
@@ -1,28 +1,60 @@
-var orientation = Camera.getOrientation();
-orientation = Quat.safeEulerAngles(orientation);
-orientation.x = 0;
-orientation = Quat.fromVec3Degrees(orientation);
-var center = Vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getFront(orientation)));
+var center = Vec3.sum(Vec3.sum(MyAvatar.position, {
+    x: 0,
+    y: 0.5,
+    z: 0
+}), Vec3.multiply(2, Quat.getForward(Camera.getOrientation())));
 
 var SCRIPT_URL = Script.resolvePath("magicWandEntityScript.js");
-var MODEL_URL = Script.resolvePath("https://rawgit.com/seanjones2848/virtual_goodies/master/Objects/magic_wand/magic_wand.fbx");
+var MODEL_URL = Script.resolvePath("magic_wand.fbx");
 var magicWand = Entities.addEntity({
     type: "Model",
     name: "magic-wand",
+    modelURL: MODEL_URL,
     position: center,
     dimentions: {
         x: 1.0,
         y: 1.0,
         z: 1.0
     },
+    gravity: {
+        x: 0,
+        y: -9.8,
+        z: 0
+    },
     script: SCRIPT_URL,
-    userdata: JSON.stringify({
+    dynamic: true,
+    userData: JSON.stringify({
         grabbableKey: {
-            grabable: true
+            grabbable: true,
+            invertSolidWhileHeld: true,
+            wantsTrigger: true
+        },
+        wearable: {
+            joints: {
+                RightHand: [{
+                  x: 0.1177130937576294,
+                  y: 0.12922893464565277,
+                  z: 0.08307232707738876
+                }, {
+                  x: 0.4934672713279724,
+                  y: 0.3605862259864807,
+                  z: 0.6394805908203125,
+                  w: -0.4664038419723511
+                }],
+                LeftHand: [{
+                  x: 0.09151676297187805,
+                  y: 0.13639454543590546,
+                  z: 0.09354984760284424
+                }, {
+                  x: -0.19628101587295532,
+                  y: 0.6418180465698242,
+                  z: 0.2830369472503662,
+                  w: 0.6851521730422974
+                }]
+            }
         }
     })
 });
-
 function cleanup() {
     Entities.deleteEntity(magicWand);
 }
